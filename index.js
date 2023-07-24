d3fcb9f6f43d3f8f41af43ad509ca98479ddd29a
DouglasRCosta <86799132+DouglasRCosta@users.noreply.github.com>
2023-07-24 11:46:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import handler from "./src/routes/routesHandler.js";
 
 const app = http.createServer(handler);
 
-app.on("error", (err, req, res) => {
-  InternalError(req, res);
-});
+
 app.listen(80, () => {
   console.log("is running");
 });
