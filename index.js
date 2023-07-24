7161f2859b21099ef768993cdf038549dddfd495
DouglasRCosta <86799132+DouglasRCosta@users.noreply.github.com>
2023-07-24 11:45:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,3 @@ app.on("error", (err, req, res) => {
 app.listen(400, () => {
   console.log("is running");
 });
-
-const InternalError = (req, res) => {
-  res.statusCode = 400;
-  res.setHeader("Content-Type", "application/json");
-  res.write(JSON.stringify({ err: "erro Interno no servidor" }));
-  res.end();
-};
